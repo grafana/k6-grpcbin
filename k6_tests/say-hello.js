e8fa9e0dfc3d9ae412d893271209f2a7c732cb3c
Pepe Cano <825430+ppcano@users.noreply.github.com>
2023-08-23 13:48:12 +0200

    Rename `k6_tests` folder

--- k6_tests/say-hello.js
+++ k6_tests/say-hello.js
@@ -5,18 +5,11 @@ const conf = {
   baseURL: __ENV.BASE_URL || "grpcbin.test.k6.io:9001"
 }
 
+
 export let options = {
   stages: [
-    { target: 5000, duration: "5m" },
-  ],
-  ext: {
-    loadimpact: {
-      name: "grpcbin_demo.js",
-      distribution: {
-        scenarioLabel2: { loadZone: "amazon:ie:dublin", percent: 100 }
-      }
-    }
-  }
+    { target: 10, duration: "30s" },
+  ]
 };
 
 
@@ -24,6 +17,7 @@ const client = new grpc.Client();
 client.load(['definitions'], 'hello.proto');
 
 export default () => {
+  console.log('connecting: '+ conf.baseURL);
   client.connect(conf.baseURL, {
     // plaintext: false
   });
